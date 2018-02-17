02108663b953cc102a2af19440b87b887eb0d2d3
piccioli <piccioli@netseven.it>
2018-02-17 10:38:41 +0100

    Tolto debug

--- src/autoload.php
+++ src/autoload.php
@@ -7,7 +7,6 @@ $path = dirname(__FILE__);
 $d = dir($path.'/classes');
 while (false !== ($entry = $d->read())) {
 	if (preg_match('/\.php/', $entry)) {
-		echo $entry;
 		require "$path/classes/$entry";
 	}
 }
